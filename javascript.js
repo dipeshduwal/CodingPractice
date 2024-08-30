fc5367a5e5f08f1c947fed8be417abfa05e8513a
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-30 11:15:50 +0545

    Add a paragraph tag with red color text content

--- javascript.js
+++ javascript.js
@@ -5,3 +5,14 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+//add a <p> with red text that says “Hey I’m red!”
+
+const content1 = document.createElement("p");
+content1.classList.add("content1");
+content1.textContent="Hey, I'm red!";
+content1.style.color= "red";
+
+container.appendChild(content1);
+
+//an <h3> with blue text that says “I’m a blue h3!”
