10e6f7b65b15c51678178e89f72e35b600110ba9
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-04 13:49:51 +0545

    Read html files using node

--- basicInfo/index.js
+++ basicInfo/index.js
@@ -2,21 +2,31 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-//reading a page404 file
+const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+  if (err) throw err;
+  return data;
+});
 
-const page404 = fs.readFileSync("404.html", "utf-8",(err,data)=>{
-    if (err) throw err;
-    return data;
-})
-
-http.createServer(function(req,res){
+http
+  .createServer(function (req, res) {
     const q = url.parse(req.url, true);
     let filename = "";
-    if (q.pathname === "/"){
-        filename = "." + "/index.html";
+    if (q.pathname === "/") {
+      filename = "." + "/index.html";
     } else {
-        filename = "." + q.pathname;
+      filename = "." + q.pathname;
     }
-    
-})
 
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write(page404);
+        return res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      }
+    });
+  })
+  .listen(8080);
