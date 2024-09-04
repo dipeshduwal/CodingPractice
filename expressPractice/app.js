e4f9e90cc3be884d4e0250a8f9b432894b82cfb8
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-04 14:51:42 +0545

    import express then call it to initialize the app variable

---
+++ expressPractice/app.js
@@ -0,0 +1,8 @@
+//import express then call it to initialize the app variable
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
