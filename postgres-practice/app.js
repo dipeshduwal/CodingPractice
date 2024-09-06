0c1752467fccdec782c9dad2cebbf44bd4a15c01
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-06 13:54:07 +0545

    Add database folder and database connection

--- postgres-practice/app.js
+++ postgres-practice/app.js
@@ -4,8 +4,20 @@ const app = express();
 app.get("/", (req, res) => 
     console.log("usernames will be logged here - wip"));
 
-app.get("/new", (req,res,next) =>
-    res.send);
+app.get("/new", (req,res) =>
+    res.send(`
+    <html>
+      <head><title>Username Form</title></head>
+      <body>
+        <h1>Enter Username</h1>
+        <form action="/submit" method="POST">
+          <label for="username">Username:</label>
+          <input type="text" id="username" name="username" required>
+          <button type="submit">Submit</button>
+        </form>
+      </body>
+    </html>
+  `));
 
 app.post("/new", (req,res) => 
     console.log("username to be saved:", req.body.username));
